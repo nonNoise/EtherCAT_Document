614c1236362ec53fad91f3d9e9d945ebe599a85b
nonNoise <kitagami2artifactnoise.com>
2019-01-20 23:23:35 +0900

    test init

--- uplode.py
+++ uplode.py
@@ -4,20 +4,25 @@ import sys
 
 prc = pexpect.spawn("make html")
 prc.expect( pexpect.EOF )
+print(prc.before)
 
 prc = pexpect.spawn("git add -A")
 prc.expect( pexpect.EOF )
+print(prc.before)
 
 prc = pexpect.spawn("git commit -a -m \"test init\" ")
 prc.expect( pexpect.EOF )
+print(prc.before)
 
-print('Username      : ', sys.argv[1])
-print('Password      : ', sys.argv[2])
+print('Username:',sys.argv[1])
+print('Password:',sys.argv[2])
 
 prc = pexpect.spawn("git push origin master")
 prc.expect("Username for 'https://github.com':")
-prc.sendline(sys.argv[0])
-
-prc.expect("Password for 'https://nonNoise@github.com': ")
-prc.sendline(sys.argv[1])
+prc.sendline(sys.argv[0].strip())
 prc.expect( pexpect.EOF )
+print(prc.before)
+prc.expect("Password for 'https://nonNoise@github.com': ")
+prc.sendline(sys.argv[1].strip())
+prc.expect( pexpect.EOF )
+print(prc.before)
