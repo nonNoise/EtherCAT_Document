d2420a6043faa751c63f5bb241dc167ad330216c
nonNoise <kitagami2artifactnoise.com>
2019-01-20 23:14:20 +0900

    test init

--- uplode.py
+++ uplode.py
@@ -11,8 +11,8 @@ prc.expect( pexpect.EOF )
 prc = pexpect.spawn("git commit -a -m \"test init\" ")
 prc.expect( pexpect.EOF )
 
-print('Username      : ', sys.argv[0])
-print('Password      : ', sys.argv[1])
+print('Username      : ', sys.argv[1])
+print('Password      : ', sys.argv[2])
 
 prc = pexpect.spawn("git push origin master")
 prc.expect("Username for 'https://github.com':")
