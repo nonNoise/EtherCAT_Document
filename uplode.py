d03bf4cd292eb89b7d6945e7631f44930907f494
kitagami <kitagami@abund.jp>
2019-02-04 11:39:56 +0900

    更新

--- uplode.py
+++ uplode.py
@@ -1,6 +1,7 @@
 import pexpect
 import argparse
 import sys
+import time
 
 print ('Username: '+sys.argv[1])
 print ('Password: '+sys.argv[2])
@@ -11,26 +12,33 @@ print(command)
 prc = pexpect.spawn(command)
 prc.expect( pexpect.EOF )
 print(prc.before)
+time.sleep(1)
 
 command = "git add -A"
 print(command)
 prc = pexpect.spawn(command)
 prc.expect( pexpect.EOF )
 #print(prc.before)
+time.sleep(1)
+
 command = "git commit -a -m \"" + sys.argv[3]+ "\""
 print(command)
 prc = pexpect.spawn(command)
 prc.expect( pexpect.EOF )
 #print(prc.before)
+time.sleep(1)
 
 command = "git push origin master"
 print(command)
+time.sleep(1)
 prc = pexpect.spawn(command)
 prc.expect("Username for 'https://github.com':")
 prc.sendline(sys.argv[1])
+time.sleep(1)
 prc.expect("Password for 'https://"+sys.argv[1]+"@github.com':")
 prc.sendline(sys.argv[2])
 prc.expect(pexpect.EOF)
 print(prc.before)
+time.sleep(1)
 
 print("Complete !!")
